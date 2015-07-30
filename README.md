afcfe0de2c1d3eeb6275817f9b71740c9aed10fe
JP Richardson <jprichardson@gmail.com>
2015-07-30 10:58:56 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,32 @@
+Atom Rename Tabs
+================
+
+Renames your Atom Text Editor tab titles to more useful names if there are many
+instances of the same file name open. This is consistent with Sublime Text Editor tab
+title behavior.
+
+Atom's tab naming isn't very helpful if there are multiple files with the same
+name open. This package is useful if you write Node.js app with a lot of files
+named `index.js`. Atom would title the tabs `index.js - /my/project...`. That's
+not very helpful. Assuming you have two files: `/my/project/foo/index.js` and
+`/my/project/bar/index.js` the tabs to `index.js - foo` and `index.js - bar` respectively.
+
+
+Install
+-------
+
+    apm install rename-tabs
+
+or go to `Settings` → `Install` → `Search` and search for `rename-tabs`.
+
+
+Usage
+-----
+
+Just open and edit files, the renaming will happen automatically.
+
+
+License
+-------
+MIT
+Copyright (c) 2015 [JP Richardson](https://www.github.com/jprichardson)
---
+++ index.js
@@ -0,0 +1,62 @@
+var CompositeDisposable = require('atom').CompositeDisposable
+var path = require('path')
+
+/* global atom */
+
+var _disposables = new CompositeDisposable()
+
+function activate () {
+  _disposables.add(atom.workspace.observeTextEditors(function (editor) {
+    _disposables.add(editor.onDidDestroy(renameTabs))
+    _disposables.add(editor.onDidChangePath(renameTabs))
+    _disposables.add(editor.onDidChangeTitle(renameTabs))
+  }))
+  _disposables.add(atom.workspace.onDidOpen(renameTabs))
+
+  // delay in case of refresh
+  setTimeout(function () {
+    renameTabs()
+  }, 1500) // <-- atom is kinda slow starting up
+}
+
+function deactivate () {
+  _disposables.dispose()
+}
+
+function renameTabs () {
+  var elements = [].slice.call(document.querySelectorAll('li.tab .title'))
+  var names = {}
+  var tabs = elements.map(function (el) {
+    var name = el.getAttribute('data-name')
+    names[name] = name in names ? names[name] + 1 : 1
+    return {
+      name: name,
+      path: el.getAttribute('data-path'),
+      element: el
+    }
+  })
+
+  tabs.forEach(function (tab) {
+    tab.uniqueName = names[tab.name] === 1
+    setTimeout(function () {
+      renameTab(tab, tabs)
+    }, 20)
+  })
+}
+
+// todo: allow user to configure this
+function renameTab (tab, tabs) {
+  if (tab.uniqueName) {
+    tab.element.innerText = tab.name
+  } else {
+    var dir = path.dirname(tab.path)
+    var dirs = dir.split(path.sep)
+    var prevDir = dirs[dirs.length - 1]
+    tab.element.innerText = tab.name + ' - ' + prevDir
+  }
+}
+
+module.exports = {
+  activate: activate,
+  deactivate: deactivate
+}
